686e1e789b89d552674f1350c8fd02b77185ecc8
eoin <eoinmac14@gmail.com>
2016-03-25 11:04:01 +0000

    got dataframe  to json format

--- RippleExchangeV1.1-master/src/main/java/pack/RippleMain.java
+++ RippleExchangeV1.1-master/src/main/java/pack/RippleMain.java
@@ -8,57 +8,39 @@ import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
-import org.datanucleus.store.types.backed.*;
-import org.joda.time.Days;
-import org.joda.time.LocalDate;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
-import java.text.SimpleDateFormat;
+import java.sql.*;
 import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.sql.*;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
-/**
- * Created by 10318411 on 03/02/2016.
- */
+
 
 public class RippleMain {
     public JsonObject jsonObject;
-    public JsonObject getJson(){
-    return jsonObject;
-}
 
-    public static void main(String[] args) throws IOException /*try*/{
+    public static void main(String[] args) throws IOException /*try*/ {
 
-        String base ="XRP";
-        String counter ="EUR+rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq";
+        String base = "XRP";
+        String counter = "EUR+rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq";
         //to default to current date, remove date variable
-        String date="2015-01-13T19:57:00Z";
+        String date = "2015-01-13T19:57:00Z";
         //String date="2015-01-13";
-     //   Date date = new Date();
- ;      String apiMethod = "stats";
+        //   Date date = new Date();
+        String apiMethod = "stats";
         String thisMoment = DateTimeFormatter.ofPattern("yyyy-MM-dd")
                 .withZone(ZoneOffset.UTC)
                 .format(Instant.now());
         System.out.println(thisMoment);
-        ArrayList<URL> URLList= new ArrayList<URL>();
+        ArrayList<URL> URLList = new ArrayList<URL>();
         //protected String apiMethod = "exchange_rates";
 
         //select which api method you want to retrieve
-        URL rippleUrl=null;
-        ApiMethod objType=null;
+        URL rippleUrl = null;
+        ApiMethod objType = null;
         switch (apiMethod) {
             //create the link to the URL
             case ("exchange_rates"):
@@ -77,14 +59,14 @@ public class RippleMain {
                    // date1.minusMinutes(1);
                     System.out.println(str2); // 2010-01-02
                 }*/
-                objType= new ExchangeRates();
+                objType = new ExchangeRates();
                 break;
             case ("stats"):
                 //CAN GET LAST 200 RESUlTS
-                String start="2016-03-15";
-                String end="2014-08-31";
-                String interval="hour";
-                   // rippleUrl = new URL(new StringBuilder().append("https://data.ripple.com/v2/").append(apiMethod).append("/").append("?start=2016-03-15&end=2014-08-31&interval=day&family=metric&metrics=accounts_created,exchanges_count,ledger_count,payments_count").toString());
+                String start = "2016-03-15";
+                String end = "2014-08-31";
+                String interval = "hour";
+                // rippleUrl = new URL(new StringBuilder().append("https://data.ripple.com/v2/").append(apiMethod).append("/").append("?start=2016-03-15&end=2014-08-31&interval=day&family=metric&metrics=accounts_created,exchanges_count,ledger_count,payments_count").toString());
                /* for(int i=0;i<20;i++)
                 {
 
@@ -94,29 +76,30 @@ public class RippleMain {
                     URLList.add(rippleUrl);
                 }*/
                 System.out.println("2");
-                URLList=APIQueries.createQueries(apiMethod,start,end,interval);
-                objType=new Stats();
+                URLList = APIQueries.createQueries(apiMethod, start, end, interval);
+                objType = new Stats();
                 break;
 
-            default: System.out.println("no url");
+            default:
+                System.out.println("no url");
 
         }
         //Create a connection to the database
 
-        Connection myConn=null;
+        Connection myConn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo?autoReconnect=true&useSSL=false", "root", "Scorpio21*");
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-            Statement statement=null;
-            URL rippleURL=null;
+        Statement statement = null;
+        URL rippleURL = null;
         //iterate through URLS creating JSONS and storing them to a mySQL table
-     Iterator iter=URLList.iterator();
-        while(iter.hasNext()) {
+        Iterator iter = URLList.iterator();
+        while (iter.hasNext()) {
             rippleUrl = (URL) iter.next();
 
             //parsing the json from the url
@@ -159,34 +142,60 @@ public class RippleMain {
         //reading from mysql to spark dataframe
         //"jdbc:mysql://localhost:3306/demo?autoReconnect=true&useSSL=false", "root", "Scorpio21*"
         Map<String, String> options = new HashMap<String, String>();
-        options.put("url","jdbc:mysql://localhost:3306/demo?autoReconnect=true&useSSL=false&user=root&password=Scorpio21*");
+        options.put("url", "jdbc:mysql://localhost:3306/demo?autoReconnect=true&useSSL=false&user=root&password=Scorpio21*");
         options.put("dbtable", "test");
         JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("DBConnection").setMaster("local[*]"));
         SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
         // DataFrame jdbcDF = sqlContext.load("jdbc", options).cache();
-        DataFrame jdbcDF = sqlContext.jdbc(options.get("url"),options.get("dbtable"));
-       jdbcDF = jdbcDF.select("*").where(jdbcDF.col("exchanges").isNotNull());
-        RDD h= jdbcDF.rdd();
+        DataFrame jdbcDF = sqlContext.jdbc(options.get("url"), options.get("dbtable"));
+        jdbcDF = jdbcDF.select("*").where(jdbcDF.col("exchanges").isNotNull());
+
+
+        RDD h = jdbcDF.rdd();
         //JavaRDD gg=jdbcDF.toJavaRDD();
         Row[] rows = jdbcDF.collect();
-        List<String> data=new LinkedList<>();
+        List<String> data = new LinkedList<>();
         for (Row row2 : rows) {
             data.add(row2.toString());
         }
 
         JavaRDD<String> distData = sc.parallelize(data);
-        DataFrame df=sqlContext.read().json(distData);
+        DataFrame df = sqlContext.read().json(distData);
         //DataFrame jdbcDF2=sqlContext.read().json(h);
-      // DataFrame jdbcDF2= sqlContext.read().json(jdbcDF.select("*").where(jdbcDF.col("exchanges").isNotNull()));
+        // DataFrame jdbcDF2= sqlContext.read().json(jdbcDF.select("*").where(jdbcDF.col("exchanges").isNotNull()));
        /* System.out.println("Data------------------->" + jdbcDF.toJSON().first());
         Row[] rows = jdbcDF.collect();
         System.out.println("Without Filter \n ------------------------------------------------- ");
         for (Row row2 : rows) {
             System.out.println(row2.toString());
         }*/
-        jdbcDF.show();
-        df.show();
+        DataFrame personPositions = df.select(df.col("stats").as("stat"),
+                org.apache.spark.sql.functions.explode(df.col("stats")).as("stat1"));
+
+        DataFrame test = personPositions.select(
+                personPositions.col("stat1").getField("accounts_created").as("accounts"), personPositions.col("stat1").getField("exchanges_count").as("exchanges"), personPositions.col("stat1").getField("ledger_count").as("ledger"), personPositions.col("stat1").getField("payments_count").as("payments"), personPositions.col("stat1").getField("date").as("date"));
+
+        test.show();
+
+        jdbcDF.drop("count").drop("result").drop("rate").drop("marker").show();
+        jdbcDF.drop("count").drop("result").drop("rate").drop("marker").schema();
+
+
+        df.drop("count").drop("marker").drop("rate").drop("result").show();
+        df.drop("count").drop("marker").drop("rate").drop("result").schema();
+
+
+        //Row[] statist=jdbcDF.select("stats").collect();
+
+
+        // System.out.println(statist.toString());
+        /*
+        // Compute column summary statistics.
+        MultivariateStatisticalSummary summary = org.apache.spark.mllib.stat.Statistics.colStats(distData.rdd());
+        System.out.println(summary.mean()); // a dense vector containing the mean value for each column
+        System.out.println(summary.variance()); // column-wise variance
+        System.out.println(summary.numNonzeros()); // number of nonzeros in each column*/
         //jdbcDF2.show();
        /* System.out.println("Filter Data\n ------------------------------------------------- ");
         jdbcDF = jdbcDF.select("*").where(jdbcDF.col("exchanges").isNotNull());
@@ -198,17 +207,17 @@ public class RippleMain {
         try {
             if (statement != null)
                 statement.close();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             e.getErrorCode();
         }
         try {
             if (myConn != null)
-               myConn.close();
-        }catch (SQLException p){
+                myConn.close();
+        } catch (SQLException p) {
             p.getErrorCode();
         }
 //            Json2SparkSQL test= new Json2SparkSQL();
-  //     test.dataFrame();
+        //     test.dataFrame();
         //call the method from the pojo
         //System.out.println(Arrays.deepToString(RippleExchange.toPojo(jsonObject,objType).getStats()));
         //------System.out.println(Arrays.deepToString(RippleExchange.toPojo(jsonObject,objType).getStats()));
@@ -297,6 +306,10 @@ public class RippleMain {
     }/*catch(JsonFromURL e) {
 
     }*/
+
+    public JsonObject getJson(){
+    return jsonObject;
+}
 
 }
 
